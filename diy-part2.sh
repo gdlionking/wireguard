d55851c815d62e27919704647cbd4ef4755139c7
gdlionking <59407003+gdlionking@users.noreply.github.com>
2021-01-23 19:37:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,6 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
+chmod -R 777 files
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/files/bin/config_generate
