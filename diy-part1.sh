1a66d05584e21dbc3f2a7bd2db0da90cfc8a6466
gdlionking <59407003+gdlionking@users.noreply.github.com>
2021-09-13 22:43:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,16 @@
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# Uncomment a feed source
+
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
